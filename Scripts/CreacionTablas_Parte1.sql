a147a0b2900a23ef955e871fbaac0621f73bdeea
aleedca <acerdas1701@estudiantec.cr>
2022-12-26 17:26:17 -0600

    modified attributes name

--- Scripts/CreacionTablas_Parte1.sql
+++ Scripts/CreacionTablas_Parte1.sql
@@ -2,8 +2,8 @@
 -- Gender
 CREATE TABLE Gender(
     idGender NUMBER(10) PRIMARY KEY,
-    description VARCHAR(16) CONSTRAINT gender_description_nn NOT NULL,
-    user VARCHAR(16) CONSTRAINT gender_user_nn,
+    descriptionGender VARCHAR(16) CONSTRAINT gender_description_nn NOT NULL,
+    userCreation VARCHAR(16) CONSTRAINT gender_user_nn,
     lastUser VARCHAR(16) CONSTRAINT gender_lastUser_nn,
     lastDate DATE CONSTRAINT gender_lastDate_nn,
     dateCreation DATE CONSTRAINT gender_dateCreation_nn
@@ -13,7 +13,7 @@ CREATE TABLE Gender(
 CREATE TABLE TypeIdentification(
     idTypeIdentification NUMBER(10) PRIMARY KEY,
     mask VARCHAR(16) CONSTRAINT typeIdentification_mask_nn NOT NULL,
-    user VARCHAR(16) CONSTRAINT typeIdentification_user_nn,
+    userCreation VARCHAR(16) CONSTRAINT typeIdentification_user_nn,
     lastUser VARCHAR(16) CONSTRAINT typeIdentification_lastUser_nn,
     lastDate DATE CONSTRAINT typeIdentification_lastDate_nn,
     dateCreation DATE CONSTRAINT typeIdentification_dateCreation_nn
@@ -22,8 +22,8 @@ CREATE TABLE TypeIdentification(
 -- Calendar
 CREATE TABLE Calendar(
     idCalendar NUMBER(10) PRIMARY KEY,
-    description VARCHAR(16) CONSTRAINT calendar_description_nn NOT NULL,
-    user VARCHAR(16) CONSTRAINT calendar_user_nn,
+    descriptionCalendar VARCHAR(16) CONSTRAINT calendar_description_nn NOT NULL,
+    userCreation VARCHAR(16) CONSTRAINT calendar_user_nn,
     lastUser VARCHAR(16) CONSTRAINT calendar_lastUser_nn,
     lastDate DATE CONSTRAINT calendar_lastDate_nn,
     dateCreation DATE CONSTRAINT calendar_dateCreation_nn
@@ -32,8 +32,8 @@ CREATE TABLE Calendar(
 -- Continent
 CREATE TABLE Continent(
     idContinent NUMBER(10) PRIMARY KEY,
-    name VARCHAR(16) CONSTRAINT continent_name_nn NOT NULL,
-    user VARCHAR(16) CONSTRAINT continent_user_nn,
+    nameContinent VARCHAR(16) CONSTRAINT continent_name_nn NOT NULL,
+    userCreation VARCHAR(16) CONSTRAINT continent_user_nn,
     lastUser VARCHAR(16) CONSTRAINT continent_lastUser_nn,
     lastDate DATE CONSTRAINT continent_lastDate_nn,
     dateCreation DATE CONSTRAINT continent_dateCreation_nn
@@ -44,29 +44,29 @@ CREATE TABLE Event(
     idEvent NUMBER(10) PRIMARY KEY,
     idCalendar NUMBER(10) CONSTRAINT event_idCalendar_nn NOT NULL,
     typeEvent VARCHAR(16) CONSTRAINT event_typeEvent_nn NOT NULL,
-    user VARCHAR(16) CONSTRAINT event_user_nn,
+    userCreation VARCHAR(16) CONSTRAINT event_user_nn,
     lastUser VARCHAR(16) CONSTRAINT event_lastUser_nn,
     lastDate DATE CONSTRAINT event_lastDate_nn,
     dateCreation DATE CONSTRAINT event_dateCreation_nn
 );
 
 -- Group
-CREATE TABLE Group(
-    idGroup NUMBER(10) PRIMARY KEY,
-    idEvent NUMBER(10) CONSTRAINT group_idEvent_nn NOT NULL,
-    description VARCHAR(16) CONSTRAINT group_description_nn NOT NULL,
-    user VARCHAR(16) CONSTRAINT group_user_nn,
-    lastUser VARCHAR(16) CONSTRAINT group_lastUser_nn,
-    lastDate DATE CONSTRAINT group_lastDate_nn,
-    dateCreation DATE CONSTRAINT group_dateCreation_nn
+CREATE TABLE GroupEvent(
+    idGroupEvent NUMBER(10) PRIMARY KEY,
+    idEvent NUMBER(10) CONSTRAINT groupEvent_idEvent_nn NOT NULL,
+    descriptionGroupEvent VARCHAR(16) CONSTRAINT groupEvent_description_nn NOT NULL,
+    userCreation VARCHAR(16) CONSTRAINT groupEvent_user_nn,
+    lastUser VARCHAR(16) CONSTRAINT groupEvent_lastUser_nn,
+    lastDate DATE CONSTRAINT groupEvent_lastDate_nn,
+    dateCreation DATE CONSTRAINT groupEvent_dateCreation_nn
 );
 
 -- CountryTeam
 CREATE TABLE CountryTeam(
     idCountryTeam NUMBER(10) PRIMARY KEY,
     idContinent NUMBER(10) CONSTRAINT countryTeam_idContinent_nn NOT NULL,
-    name VARCHAR(16) CONSTRAINT countryTeam_name_nn NOT NULL,
-    user VARCHAR(16) CONSTRAINT countryTeam_user_nn,
+    nameCountryTeam VARCHAR(16) CONSTRAINT countryTeam_name_nn NOT NULL,
+    userCreation VARCHAR(16) CONSTRAINT countryTeam_user_nn,
     lastUser VARCHAR(16) CONSTRAINT countryTeam_lastUser_nn,
     lastDate DATE CONSTRAINT countryTeam_lastDate_nn,
     dateCreation DATE CONSTRAINT countryTeam_dateCreation_nn
@@ -76,9 +76,9 @@ CREATE TABLE CountryTeam(
 CREATE TABLE Team(
     idTeam NUMBER(10) PRIMARY KEY,
     idCountryTeam NUMBER(10) CONSTRAINT team_idCountryTeam_nn NOT NULL,
-    idGroup NUMBER(10) CONSTRAINT team_idGroup_nn NOT NULL,
-    name VARCHAR(16) CONSTRAINT team_name_nn NOT NULL,
-    user VARCHAR(16) CONSTRAINT team_user_nn,
+    idGroupEvent NUMBER(10) CONSTRAINT team_idGroupEvent_nn NOT NULL,
+    nameTeam VARCHAR(16) CONSTRAINT team_name_nn NOT NULL,
+    userCreation VARCHAR(16) CONSTRAINT team_user_nn,
     lastUser VARCHAR(16) CONSTRAINT team_lastUser_nn,
     lastDate DATE CONSTRAINT team_lastDate_nn,
     dateCreation DATE CONSTRAINT team_dateCreation_nn
@@ -94,7 +94,7 @@ CREATE TABLE GroupStats(
     goalsScored NUMBER(10) CONSTRAINT groupStats_goalsScored_nn NOT NULL,
     goalsReceived NUMBER(10) CONSTRAINT groupStats_goalsReceived_nn NOT NULL,
     fairPlayPoints NUMBER(10) CONSTRAINT groupStats_fairPlayPoints_nn NOT NULL,
-    user VARCHAR(16) CONSTRAINT groupStats_user_nn,
+    userCreation VARCHAR(16) CONSTRAINT groupStats_user_nn,
     lastUser VARCHAR(16) CONSTRAINT groupStats_lastUser_nn,
     lastDate DATE CONSTRAINT groupStats_lastDate_nn,
     dateCreation DATE CONSTRAINT groupStats_dateCreation_nn
@@ -106,7 +106,7 @@ ALTER TABLE Event
     ADD CONSTRAINT fk_event_calendar FOREIGN KEY (idCalendar) REFERENCES Calendar(idCalendar);
 
 -- FK Group-Event
-ALTER TABLE Group
+ALTER TABLE GroupEvent
     ADD CONSTRAINT fk_group_event FOREIGN KEY (idEvent) REFERENCES Event(idEvent);
 
 -- FK CountryTeam-Continent
@@ -119,7 +119,7 @@ ALTER TABLE Team
 
 -- FK Team-Group
 ALTER TABLE Team 
-    ADD CONSTRAINT fk_team_group FOREIGN KEY (idGroup) REFERENCES Group(idGroup);
+    ADD CONSTRAINT fk_team_group FOREIGN KEY (idGroupEvent) REFERENCES GroupEvent(idGroupEvent);
 
 -- FK GroupStats-Team
 ALTER TABLE GroupStats 
