0927abda6d83d8278c522cd2a4244aa0cb6124d4
aleedca <acerdas1701@estudiantec.cr>
2022-12-27 17:37:14 -0600

    added attributes and tables comments

--- Scripts/CreacionTablas_Parte1.sql
+++ Scripts/CreacionTablas_Parte1.sql
@@ -18,6 +18,32 @@ CREATE TABLE Gender(
     dateCreation DATE
 );
 
+-- Table Comment
+COMMENT ON TABLE Gender
+IS 'Repository for storing the information of gender';
+
+--------------- Comment on Attributes -------------------------------------
+COMMENT ON COLUMN Gender.idGender
+IS 'Unique identifier of the Gender Table.';
+
+COMMENT ON COLUMN Gender.descriptionGender
+IS 'Description of the gender.';
+
+-- Audit Fields 
+COMMENT ON COLUMN Gender.userCreation
+IS 'User who creates the Gender Table record.';
+
+COMMENT ON COLUMN Gender.dateCreation
+IS 'Date of creation of the Gender Table record.';
+
+COMMENT ON COLUMN Gender.lastUser
+IS 'Last user to modify a record in the Gender Table.';
+
+COMMENT ON COLUMN Gender.lastDate
+IS 'Last modification date of the record in the Gender Table.';
+
+-------------------------------------------------------------------------------------------
+
 -- TypeIdentification
 CREATE TABLE TypeIdentification(
     idTypeIdentification NUMBER(10) PRIMARY KEY,
@@ -27,6 +53,32 @@ CREATE TABLE TypeIdentification(
     lastDate DATE,
     dateCreation DATE
 );
+
+-- Table Comment
+COMMENT ON TABLE TypeIdentification
+IS 'Repository for storing the information of type identification';
+
+--------------- Comment on Attributes -------------------------------------
+COMMENT ON COLUMN TypeIdentification.idTypeIdentification
+IS 'Unique identifier of the TypeIdentification Table.';
+
+COMMENT ON COLUMN TypeIdentification.mask
+IS 'Mask of the TypeIdentification.';
+
+-- Audit Fields 
+COMMENT ON COLUMN TypeIdentification.userCreation
+IS 'User who creates the TypeIdentification Table record.';
+
+COMMENT ON COLUMN TypeIdentification.dateCreation
+IS 'Date of creation of the TypeIdentification Table record.';
+
+COMMENT ON COLUMN TypeIdentification.lastUser
+IS 'Last user to modify a record in the TypeIdentification Table.';
+
+COMMENT ON COLUMN TypeIdentification.lastDate
+IS 'Last modification date of the record in the TypeIdentification Table.';
+
+-------------------------------------------------------------------------------------------
 
 -- Calendar
 CREATE TABLE Calendar(
@@ -38,6 +90,32 @@ CREATE TABLE Calendar(
     dateCreation DATE
 );
 
+-- Table Comment
+COMMENT ON TABLE Calendar
+IS 'Repository for storing the information of calendar';
+
+--------------- Comment on Attributes -------------------------------------
+COMMENT ON COLUMN Calendar.idCalendar
+IS 'Unique identifier of the Calendar Table.';
+
+COMMENT ON COLUMN Calendar.descriptionCalendar
+IS 'Description of the calendar.';
+
+-- Audit Fields 
+COMMENT ON COLUMN Calendar.userCreation
+IS 'User who creates the Calendar Table record.';
+
+COMMENT ON COLUMN Calendar.dateCreation
+IS 'Date of creation of the Calendar Table record.';
+
+COMMENT ON COLUMN Calendar.lastUser
+IS 'Last user to modify a record in the Calendar Table.';
+
+COMMENT ON COLUMN Calendar.lastDate
+IS 'Last modification date of the record in the Calendar Table.';
+
+-------------------------------------------------------------------------------------------
+
 -- Continent
 CREATE TABLE Continent(
     idContinent NUMBER(10) PRIMARY KEY,
@@ -47,6 +125,32 @@ CREATE TABLE Continent(
     lastDate DATE,
     dateCreation DATE
 );
+
+-- Table Comment
+COMMENT ON TABLE Continent
+IS 'Repository for storing the information of continent';
+
+--------------- Comment on Attributes -------------------------------------
+COMMENT ON COLUMN Continent.idContinent
+IS 'Unique identifier of the Continent Table.';
+
+COMMENT ON COLUMN Continent.nameContinent
+IS 'Name of the continent.';
+
+-- Audit Fields 
+COMMENT ON COLUMN Continent.userCreation
+IS 'User who creates the Continent Table record.';
+
+COMMENT ON COLUMN Continent.dateCreation
+IS 'Date of creation of the Continent Table record.';
+
+COMMENT ON COLUMN Continent.lastUser
+IS 'Last user to modify a record in the Continent Table.';
+
+COMMENT ON COLUMN Continent.lastDate
+IS 'Last modification date of the record in the Continent Table.';
+
+-------------------------------------------------------------------------------------------
 
 -- Event
 CREATE TABLE Event(
@@ -59,6 +163,35 @@ CREATE TABLE Event(
     dateCreation DATE
 );
 
+-- Table Comment
+COMMENT ON TABLE Event
+IS 'Repository for storing the information of the event';
+
+--------------- Comment on Attributes -------------------------------------
+COMMENT ON COLUMN Event.idEvent
+IS 'Unique identifier of the Event Table.';
+
+COMMENT ON COLUMN Event.idCalendar
+IS 'Reference to Calendar Table.';
+
+COMMENT ON COLUMN Event.typeEvent
+IS 'Type of event.';
+
+-- Audit Fields 
+COMMENT ON COLUMN Event.userCreation
+IS 'User who creates the Event Table record.';
+
+COMMENT ON COLUMN Event.dateCreation
+IS 'Date of creation of the Event Table record.';
+
+COMMENT ON COLUMN Event.lastUser
+IS 'Last user to modify a record in the Event Table.';
+
+COMMENT ON COLUMN Event.lastDate
+IS 'Last modification date of the record in the Event Table.';
+
+-------------------------------------------------------------------------------------------
+
 -- Group
 CREATE TABLE GroupEvent(
     idGroupEvent NUMBER(10) PRIMARY KEY,
@@ -70,6 +203,35 @@ CREATE TABLE GroupEvent(
     dateCreation DATE
 );
 
+-- Table Comment
+COMMENT ON TABLE GroupEvent
+IS 'Repository for storing the information of the group of each event';
+
+--------------- Comment on Attributes -------------------------------------
+COMMENT ON COLUMN GroupEvent.idGroupEvent
+IS 'Unique identifier of the GroupEvent Table.';
+
+COMMENT ON COLUMN GroupEvent.idEvent
+IS 'Reference to Event Table.';
+
+COMMENT ON COLUMN GroupEvent.descriptionGroupEvent
+IS 'Description of the group of each event.';
+
+-- Audit Fields 
+COMMENT ON COLUMN GroupEvent.userCreation
+IS 'User who creates the GroupEvent Table record.';
+
+COMMENT ON COLUMN GroupEvent.dateCreation
+IS 'Date of creation of the GroupEvent Table record.';
+
+COMMENT ON COLUMN GroupEvent.lastUser
+IS 'Last user to modify a record in the GroupEvent Table.';
+
+COMMENT ON COLUMN GroupEvent.lastDate
+IS 'Last modification date of the record in the GroupEvent Table.';
+
+-------------------------------------------------------------------------------------------
+
 -- CountryTeam
 CREATE TABLE CountryTeam(
     idCountryTeam NUMBER(10) PRIMARY KEY,
@@ -79,6 +241,36 @@ CREATE TABLE CountryTeam(
     lastUser VARCHAR(16),
     lastDate DATE,
     dateCreation DATE
+);
+
+-- Table Comment
+COMMENT ON TABLE CountryTeam
+IS 'Repository for storing the information of the country team';
+
+--------------- Comment on Attributes -------------------------------------
+COMMENT ON COLUMN CountryTeam.idCountryTeam
+IS 'Unique identifier of the CountryTeam Table.';
+
+COMMENT ON COLUMN CountryTeam.idContinent
+IS 'Reference to Continent Table.';
+
+COMMENT ON COLUMN CountryTeam.nameCountryTeam
+IS 'Name of the country of the team.';
+
+-- Audit Fields 
+COMMENT ON COLUMN CountryTeam.userCreation
+IS 'User who creates the CountryTeam Table record.';
+
+COMMENT ON COLUMN CountryTeam.dateCreation
+IS 'Date of creation of the CountryTeam Table record.';
+
+COMMENT ON COLUMN CountryTeam.lastUser
+IS 'Last user to modify a record in the CountryTeam Table.';
+
+COMMENT ON COLUMN CountryTeam.lastDate
+IS 'Last modification date of the record in the CountryTeam Table.';
+
+-------------------------------------------------------------------------------------------
 
 -- Team
 CREATE TABLE Team(
@@ -91,6 +283,38 @@ CREATE TABLE Team(
     lastDate DATE,
     dateCreation DATE
 );
+
+-- Table Comment
+COMMENT ON TABLE Team
+IS 'Repository for storing the information of the team';
+
+--------------- Comment on Attributes -------------------------------------
+COMMENT ON COLUMN Team.idTeam
+IS 'Unique identifier of the Team Table.';
+
+COMMENT ON COLUMN Team.idCountryTeam
+IS 'Reference to CountryTeam Table.';
+
+COMMENT ON COLUMN Team.idGroupEvent
+IS 'Reference to GroupEvent Table.';
+
+COMMENT ON COLUMN Team.nameTeam
+IS 'Name of the team.';
+
+-- Audit Fields 
+COMMENT ON COLUMN Team.userCreation
+IS 'User who creates the Team Table record.';
+
+COMMENT ON COLUMN Team.dateCreation
+IS 'Date of creation of the Team Table record.';
+
+COMMENT ON COLUMN Team.lastUser
+IS 'Last user to modify a record in the Team Table.';
+
+COMMENT ON COLUMN Team.lastDate
+IS 'Last modification date of the record in the Team Table.';
+
+-------------------------------------------------------------------------------------------
 
 -- GroupStats
 CREATE TABLE GroupStats(
@@ -108,6 +332,50 @@ CREATE TABLE GroupStats(
     dateCreation DATE
 );
 
+-- Table Comment
+COMMENT ON TABLE GroupStats
+IS 'Repository for storing the information of the stats of each team on depending on their group';
+
+--------------- Comment on Attributes -------------------------------------
+COMMENT ON COLUMN GroupStats.idStats
+IS 'Unique identifier of the GroupStats Table.';
+
+COMMENT ON COLUMN GroupStats.idTeam
+IS 'Reference to Team Table.';
+
+COMMENT ON COLUMN GroupStats.wonMatches
+IS 'Amount of won matches of the team on their group';
+
+COMMENT ON COLUMN GroupStats.tiedMatches
+IS 'Amount of tied matches of the team on their group';
+
+COMMENT ON COLUMN GroupStats.lostMatches
+IS 'Amount of lost matches of the team on their group';
+
+COMMENT ON COLUMN GroupStats.goalsScored
+IS 'Amount of goals scored of the team on their group';
+
+COMMENT ON COLUMN GroupStats.goalsReceived
+IS 'Amount of goals received of the team on their group';
+
+COMMENT ON COLUMN GroupStats.fairPlayPoints
+IS 'Amount of fair play points of the team on their group';
+
+-- Audit Fields 
+COMMENT ON COLUMN GroupStats.userCreation
+IS 'User who creates the GroupStats Table record.';
+
+COMMENT ON COLUMN GroupStats.dateCreation
+IS 'Date of creation of the GroupStats Table record.';
+
+COMMENT ON COLUMN GroupStats.lastUser
+IS 'Last user to modify a record in the GroupStats Table.';
+
+COMMENT ON COLUMN GroupStats.lastDate
+IS 'Last modification date of the record in the GroupStats Table.';
+
+-------------------------------------------------------------------------------------------
+
 CREATE TABLE SoccerMatchXStadium(
     idSoccerMatchXStadium NUMBER(10) PRIMARY KEY,
     idSoccerMatch NUMBER(10) CONSTRAINT soccerMatchXStadium_idSoccerMatch_nn NOT NULL,
@@ -118,7 +386,36 @@ CREATE TABLE SoccerMatchXStadium(
     dateCreation DATE
 );
 
--- ////////////////////////////////////// FOREING KEYS PARTE 1////////////////////////////////////
+-- Table Comment
+COMMENT ON TABLE SoccerMatchXStadium
+IS 'Repository for storing the information about the relationship between soccerMatch and stadium';
+
+--------------- Comment on Attributes -------------------------------------
+COMMENT ON COLUMN SoccerMatchXStadium.idSoccerMatchXStadium
+IS 'Unique identifier of the SoccerMatchXStadium Table.';
+
+COMMENT ON COLUMN SoccerMatchXStadium.idSoccerMatch
+IS 'Reference to SoccerMatch Table.';
+
+COMMENT ON COLUMN SoccerMatchXStadium.idStadium
+IS 'Reference to Stadium Table.';
+
+-- Audit Fields 
+COMMENT ON COLUMN SoccerMatchXStadium.userCreation
+IS 'User who creates the SoccerMatchXStadium Table record.';
+
+COMMENT ON COLUMN SoccerMatchXStadium.dateCreation
+IS 'Date of creation of the SoccerMatchXStadium Table record.';
+
+COMMENT ON COLUMN SoccerMatchXStadium.lastUser
+IS 'Last user to modify a record in the SoccerMatchXStadium Table.';
+
+COMMENT ON COLUMN SoccerMatchXStadium.lastDate
+IS 'Last modification date of the record in the SoccerMatchXStadium Table.';
+
+-------------------------------------------------------------------------------------------
+
+-- ////////////////////////////////////// FOREING KEYS PART 1////////////////////////////////////
 -- FK Event-Calendar
 ALTER TABLE Event
     ADD CONSTRAINT fk_event_calendar FOREIGN KEY (idCalendar) REFERENCES Calendar(idCalendar);
