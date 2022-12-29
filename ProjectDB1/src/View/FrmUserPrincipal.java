f590557b8d18544d5185a893af12a41ea65e663a
Isaac <isaacarayabusiness@hotmail.com>
2022-12-29 17:13:49 -0600

    UI updated

--- ProjectDB1/src/View/FrmUserPrincipal.java
+++ ProjectDB1/src/View/FrmUserPrincipal.java
@@ -32,11 +32,11 @@ public class FrmUserPrincipal extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 814, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 516, Short.MAX_VALUE)
         );
 
         pack();
