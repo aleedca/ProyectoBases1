06ab3335290f4ff486a2b7c53f49af3861b01cdc
Naho <nahomibv@gmail.com>
2023-01-13 13:22:12 -0600

    Update Admin Matches

--- ProjectDB1/src/View/JF_AdminMatches.java
+++ ProjectDB1/src/View/JF_AdminMatches.java
@@ -34,16 +34,15 @@ public class JF_AdminMatches extends javax.swing.JFrame {
         lblLogo = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
         btnAdminScheduledMatch = new javax.swing.JButton();
-        btnAdminNewMatch = new javax.swing.JButton();
         btnScheduleMatch = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setMinimumSize(new java.awt.Dimension(1280, 720));
+        setMinimumSize(new java.awt.Dimension(800, 720));
         setResizable(false);
 
         jPanel1.setBackground(new java.awt.Color(86, 4, 44));
-        jPanel1.setMaximumSize(new java.awt.Dimension(1280, 720));
-        jPanel1.setMinimumSize(new java.awt.Dimension(1280, 720));
+        jPanel1.setMaximumSize(new java.awt.Dimension(1000, 720));
+        jPanel1.setMinimumSize(new java.awt.Dimension(1000, 720));
         jPanel1.setPreferredSize(new java.awt.Dimension(1280, 720));
         jPanel1.setLayout(null);
 
@@ -63,11 +62,11 @@ public class JF_AdminMatches extends javax.swing.JFrame {
         lblAdminPartidos.setForeground(new java.awt.Color(255, 255, 255));
         lblAdminPartidos.setText("Administrar Partidos");
         jPanel1.add(lblAdminPartidos);
-        lblAdminPartidos.setBounds(100, 60, 521, 48);
+        lblAdminPartidos.setBounds(80, 60, 521, 48);
 
         lblLogo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/logo.png"))); // NOI18N
         jPanel1.add(lblLogo);
-        lblLogo.setBounds(820, 0, 200, 125);
+        lblLogo.setBounds(600, 0, 200, 125);
 
         jButton1.setBackground(new java.awt.Color(255, 255, 255));
         jButton1.setFont(new java.awt.Font("Yu Gothic UI Semilight", 1, 18)); // NOI18N
@@ -79,7 +78,7 @@ public class JF_AdminMatches extends javax.swing.JFrame {
             }
         });
         jPanel1.add(jButton1);
-        jButton1.setBounds(150, 210, 120, 80);
+        jButton1.setBounds(340, 170, 120, 80);
 
         btnAdminScheduledMatch.setFont(new java.awt.Font("Yu Gothic UI Semilight", 1, 18)); // NOI18N
         btnAdminScheduledMatch.setText("Administrar partido programado");
@@ -89,17 +88,7 @@ public class JF_AdminMatches extends javax.swing.JFrame {
             }
         });
         jPanel1.add(btnAdminScheduledMatch);
-        btnAdminScheduledMatch.setBounds(660, 320, 350, 90);
-
-        btnAdminNewMatch.setFont(new java.awt.Font("Yu Gothic UI Semilight", 1, 18)); // NOI18N
-        btnAdminNewMatch.setText("Administrar partido nuevo");
-        btnAdminNewMatch.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnAdminNewMatchActionPerformed(evt);
-            }
-        });
-        jPanel1.add(btnAdminNewMatch);
-        btnAdminNewMatch.setBounds(660, 440, 350, 90);
+        btnAdminScheduledMatch.setBounds(220, 430, 350, 90);
 
         btnScheduleMatch.setFont(new java.awt.Font("Yu Gothic UI Semilight", 1, 18)); // NOI18N
         btnScheduleMatch.setText("Programar partido");
@@ -109,13 +98,13 @@ public class JF_AdminMatches extends javax.swing.JFrame {
             }
         });
         jPanel1.add(btnScheduleMatch);
-        btnScheduleMatch.setBounds(660, 200, 350, 90);
+        btnScheduleMatch.setBounds(220, 290, 350, 90);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 1017, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 800, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -147,12 +136,6 @@ public class JF_AdminMatches extends javax.swing.JFrame {
         
         adminScheduledMatch.setVisible(true);
     }//GEN-LAST:event_btnAdminScheduledMatchActionPerformed
-
-    private void btnAdminNewMatchActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAdminNewMatchActionPerformed
-        JF_AdminNewMatch adminNewMatch = new  JF_AdminNewMatch();
-        
-        adminNewMatch.setVisible(true);
-    }//GEN-LAST:event_btnAdminNewMatchActionPerformed
 
     /**
      * @param args the command line arguments
@@ -190,7 +173,6 @@ public class JF_AdminMatches extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnAdminNewMatch;
     private javax.swing.JButton btnAdminScheduledMatch;
     private javax.swing.JButton btnBack;
     private javax.swing.JButton btnScheduleMatch;
