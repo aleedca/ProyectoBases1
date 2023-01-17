33a88e04b420aa2d7fe331d74e43c0dbb15a8e9f
Mariana <mfernandezmar3@gmail.com>
2023-01-17 16:11:04 -0600

    Update Validations.sql

--- Scripts/Validations.sql
+++ Scripts/Validations.sql
@@ -58,27 +58,27 @@ END validateUserAlreadyExists;
 
 
 
-CREATE OR REPLACE PROCEDURE validateIdentification(pidentification IN NUMBER,codResult OUT NUMBER) 
+CREATE OR REPLACE PROCEDURE validateIdentAlreadyExists(pidentification IN NUMBER,codResult OUT NUMBER) 
 IS
-vnIdentification NUMBER(10);
+vnIdPerson NUMBER(10);
 BEGIN
     BEGIN
-        SELECT Person.identification
-        INTO vnIdentification
+        SELECT Person.idPerson 
+        INTO vnIdPerson
         FROM Person
-        WHERE Person.idPerson = ;
+        WHERE Person.identification = identification;
     EXCEPTION
     WHEN OTHERS THEN
         ROLLBACK;
-        vnIdentification:=-1; 
+        vnIdPerson:=-1; 
     END;
 
-    IF(vnIdentification != -1) 
+    IF(vnIdPerson != -1) 
     THEN
         codResult := 0;
     ELSE
          codResult := 1;
     END IF; 
-END validateIdentification;
+END validateIdentAlreadyExists;
 
             
