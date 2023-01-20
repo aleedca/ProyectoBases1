6d5a0192d10caf2b2c9678d6fa373ed9e1b71ce0
Mariana <mfernandezmar3@gmail.com>
2023-01-19 19:59:37 -0600

    Update Prueba.sql

--- Scripts/Prueba.sql
+++ Scripts/Prueba.sql
@@ -5,13 +5,19 @@ BEGIN
     FROM Continent;
 END getContinent;
 
-
+CREATE OR REPLACE PROCEDURE insertTeamXGroup (pidTeam IN NUMBER, pidGroupEvent IN NUMBER) AS
+BEGIN
+    INSERT INTO TeamXGroup (idTeamXGroup, idTeam, idGroupEvent,userCreation, lastUser, lastDate, dateCreation)
+    VALUES (s_teamxgroup.nextval, pidTeam, pidGroupEvent,NULL, NULL, NULL, NULL);
+    COMMIT;
+END insertTeamXGroup;
 
 
 --- curContinent OUT SYS_REFCURSOR
 CREATE OR REPLACE PROCEDURE generateGroup IS
 vnIdMax NUMBER(2);
 vnIdMin NUMBER(1);
+vnCantidad NUMBER(1);
 vnNumber NUMBER(2);
 BEGIN
     SELECT MIN(idTeam) 
@@ -29,6 +35,15 @@ BEGIN
         INTO vnNumber
         FROM dual;
         DBMS_OUTPUT.PUT_LINE(vnNumber);
+        
+        SELECT COUNT(*) INTO vnCantidad
+        FROM TeamXGroup WHERE idTeam = vnNumber;
+        /*
+        IF vnCantidad = 0
+        THEN
+            insertTeamXGroup (vnNumber, pidGroupEvent IN NUMBER)
+        END IF;*/
+           
     END LOOP;
 END generateGroup;
 
